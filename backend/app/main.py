9aa80811396911590c4b4e147c95885c5df46cc3
shanjida101 <azim15-4708@diu.edu.bd>
2025-11-07 20:36:03 +0600

    Update backend main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://192.168.0.103:3000",   # ← your device’s frontend URL
+    "http://192.168.0.103:3000", 
+       "https://grow-ai-u6gh.vercel.app",     # ← your device’s frontend URL
 ]
 
 app.add_middleware(
